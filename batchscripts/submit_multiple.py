6c8e99919e699433799ae17f9755666231bd24a9
Tom Hodson <thomas.c.hodson@gmail.com>
2020-07-16 11:23:21 +0100

    batchscript updates

--- batchscripts/submit_multiple.py
+++ batchscripts/submit_multiple.py
@@ -9,8 +9,26 @@ from CX1_batch_functions import CX1job
 from CMTH_batch_functions import CMTHjob
 import time
 from munch import Munch
-import numpy as np
+import argparse
 
+def rel2home(p):
+    'if a path is relative to home, it prints it as ~/...'
+    homes = ['/rdsgpfs/general/user/tch14/home', '/rds/general/user/tch14/home']
+    for home in homes:
+        try: return '~' / p.relative_to(home)
+        except ValueError: pass
+    return p
+   
+
+parser = argparse.ArgumentParser(description='Submit multiple jobs with dependancies chained together.')
+
+parser.add_argument('script', help='The ipynb script to use.
+parser.add_argument('-o', '--out', help='The output folder name, also used as the job name.')
+parser.add_argument('-d', '--debug', action = 'store_true', default = False, help='an integer for the accumulator')
+args = parser.parse_args()
+print(args)
+sys.exit()
+    
 ### find the python script
 debug = False
 if len(argv) == 2:
@@ -18,8 +36,9 @@ if len(argv) == 2:
     job_folder_name = None
 if len(argv) == 3:
     ipynb_script = Path(argv[1]).resolve()
-    job_folder_name = Path(argv[2]).resolve()
-    
+    job_folder_name = Path(argv[2])
+    if not job_folder_name.is_absolute():
+        job_folder_name = (Path.home() / 'HPC_data' / job_folder_name)
 if len(argv) == 4:
     ipynb_script = Path(argv[1]).resolve()
     job_folder_name = Path(argv[2]).resolve()
@@ -38,9 +57,14 @@ print(f'Detected platform as {platform}')
 JobClass = CMTHjob if platform == 'CMTH' else CX1job
     
 ### Get the name of the job
-job_name = input('Give this job a name: ')
-if job_folder_name == None: job_folder_name = f'{job_name}_{time.time():.0f}'
-print(f'Job folder will be {job_folder_name}')
+if job_folder_name == None:
+    job_name = input('Give this job a name: ')
+    job_folder_name = f'{job_name}_{time.time():.0f}'
+else:
+    job_name = job_folder_name.stem
+
+print(f'Python Script: {rel2home(ipynb_script)}') 
+print(f'Job folder will be {rel2home(job_folder_name)}')
     
 ### Regenerate the py from the ipynb based on timestamps
 if not py_script.exists() or (ipynb_script.stat().st_mtime > py_script.stat().st_mtime):
@@ -63,12 +87,16 @@ except indexError:
 batch_params = Munch(context.batch_params)
 
 ### make the job which gives access to some platform specific info like paths and such
+if debug: 
+    print('debug mode, only doing 1 chain extension')
+    batch_params.chain_exts = batch_params.chain_exts[:2]
+                    
 jobs = [None for _ in batch_params.chain_exts]
 for i in batch_params.chain_exts:
     indices = (i * batch_params.total_jobs, (i+1) * batch_params.total_jobs)
     print(f'Starting job with indices {indices}')
-    if input('Ok? (y/n):') == 'n': exit()
-    jobs[i] = JobClass(py_script, job_name, job_folder_name, indices,
+    this_job_name = f"{job_name[:11]}_{i}"
+    jobs[i] = JobClass(py_script, this_job_name, job_folder_name, indices,
                        startafter = jobs[i-1] if i > 0 else None, debug = debug)
     
     
@@ -96,9 +124,11 @@ with run_desc.open('a') as f:
 print(f'See logs with: cat {str(jobs[0].submit_dir)}/logs/*')
 #print(f'see erring jobs with find ~/data/slurm_runs/{jobnum}/logs/ -not -empty -name "*.err" | sort')
 
+#do this at the very end so that 
+if input('Submit the above jobs? (n to abort):') == 'n': exit()
 
+print('Submitting jobs')
 for j in jobs: j.submit(held = False)
-
    
 
 
