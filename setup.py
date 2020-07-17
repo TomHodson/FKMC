0f30dca3a765160d5c5f0aff991671fefbf1f427
Tom <tch14@ic.ac.uk>
2020-07-17 17:26:18 +0100

    Update setup.py to remove cython

--- setup.py
+++ setup.py
@@ -1,41 +1,18 @@
 from setuptools import setup, Extension, find_packages
-from Cython.Build import cythonize
 import numpy
 import os
 
-os.environ["CC"] = "icc"
-os.environ["LDSHARED"] = "icc -shared"
 
-
-extensions = [
-    Extension("FKMC/*", ["FKMC/*.pyx"],
-        include_dirs=[numpy.get_include()],
-        extra_compile_args = ['-ip_no_inlining', '-xhost', '-qopenmp'],
-        extra_link_args = [],
-        libraries=['mkl_intel_ilp64', 'mkl_intel_thread', 'mkl_core', 'iomp5', 'pthread', 'm', 'dl'],
-        #library_dirs=["${MKLROOT}/lib/intel64"], # Doesn't seem necessary for either machine
-        ),
-]
 
 setup(
     name="FKMC",
-    version='0.2',
+    version='0.5',
     description='Falikov Kimball simulations',
     author='Tom Hodson',
     author_email='tch14@iac.ac.uk',
     packages=find_packages(),
     include_package_data=True,
-    data_files=[
-        ('',['FKMC/CMTH_runscript.sh']),
-        ('',['FKMC/CX1_runscript.sh']),
-    ],
     zip_safe = False,
-    ext_modules=cythonize(extensions, annotate=True, language_level=3),
-    install_requires=['Click'],
-    entry_points='''
-        [console_scripts]
-        run_mcmc=FKMC.jobmanager:run_mcmc_command
-    ''',
 )
 
 
