2693e1656dcf44fd9dfd0d6a9d568b943eeec219
Tom Hodson <thomas.c.hodson@gmail.com>
2020-07-20 18:37:33 +0100

    general fixes to varyingT

--- batchscripts/pure_python/varyingT.py
+++ batchscripts/pure_python/varyingT.py
@@ -1,7 +1,7 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 '''
@@ -10,8 +10,6 @@ A script to do a sweep over temperature to make:
     - the IPR and DOS color plots showing the band opening
     
 TODO:
-put the debug logic back in
-allow repeats to start over
 
 a repeat of ## data/slurm_runs/103867 but with 1e5 steps and alpha = 3 to show short range behaviour
 For the band opening plot for short ranged. It's a fixed J=5, U=1, alpha = 3, with T from 0.1 to 4 with 200 steps
@@ -33,9 +31,9 @@ Ham_params = Munch(
 print('Ham_params: ', ' '.join(f'{k}={v},' for k,v in Ham_params.items()))
 
 ########## Variable Hamiltonian parameters ###########################################################################
-chain_exts = np.arange(5) #the number of times to extend the chain maximum is about 50 on CX1
-N_steps = int(1e3) #the number of MCMC steps in each individual task
-thin = 10
+chain_exts = np.arange(1,3) #the number of times to extend the chain maximum is about 50 on CX1
+N_steps = int(100) #the number of MCMC steps in each individual task
+thin = 1
 print(f'''
 Tasks per chain: {chain_exts.size},
 Each doing {N_steps} steps,
@@ -43,11 +41,11 @@ Each doing {N_steps} steps,
 {chain_exts.size*N_steps // thin} samples,
 ''')
 
-Ts = np.linspace(0.1, 4, 200)
-Ns = np.logspace(np.log10(70), np.log10(270), 10, dtype = np.int) // 10 * 10
+Ts = np.linspace(0.1, 4, 50)
+Ns = np.logspace(np.log10(70), np.log10(300), 10, dtype = np.int) // 10 * 10
 
-structure_names = ['chain_exts', 'Ts'] #Ns is dealt with separately
-structure_dimensions = [chain_exts, Ts]
+structure_names = ['Ts',] #Ns and chain_exts is dealt with separately
+structure_dimensions = [Ts,]
 
 ########## Monte Carlo parameters ###########################################################################
 from FKMC.montecarlo import Eigenspectrum_IPR_all, p_multi_site_uniform_reflect, perturbation_accept
@@ -60,12 +58,12 @@ initial_states = Munch(
 )
 
 state_factory = initial_states.CDW1
+def loggerfactory(): return Eigenspectrum_IPR_all(bins = 2000, limit = 5)
 
 MCMC_params = Munch(
         N_steps = N_steps,
         N_burn_in = N_steps,
         thin = thin,
-        logger = Eigenspectrum_IPR_all(bins = 2000, limit = 5),
         proposal = p_multi_site_uniform_reflect,
         accept_function = perturbation_accept,
         warnings = False,
@@ -98,19 +96,25 @@ import shutil
 from FKMC.import_funcs import timefmt
 
 print('Getting environment variables')
+debug = (os.getenv('DEBUG', 'True') == 'True')
+submit_dir = Path(os.getenv('SUBMIT_DIR', Path('~/HPC_data/test/').expanduser()))
+
 job_id = int(os.getenv('JOB_ID', -1))
-debug = (os.getenv('DEBUG', job_id == -1) == 'True')
+chain_id = int(os.getenv('CHAIN_ID', -1))
 task_id = int(os.getenv('TASK_ID', 0))
-submit_dir = Path(os.getenv('SUBMIT_DIR', '/home/tch14/HPC_data/test/'))
-print(f'job_id = {job_id}, task_id = {task_id}, submit_dir = {submit_dir}')
+
+print(f'job_id = {job_id}, chain_id = {chain_id}, task_id = {task_id}, submit_dir = {submit_dir}')
 
 Is_todo = np.arange(len(Ns))
-Ns_todo = Ns.copy() 
-logs = np.empty(shape = len(Ns), dtype = object)
-if not debug and (submit_dir / 'data' / f'{task_id}.npz').exists():
-    print(f'Job file {task_id}.npz already exists')
-    print(f'Loading {task_id}.npz to retrieve the work')
-    d = Munch(np.load(submit_dir / 'data' / f'{task_id}.npz', allow_pickle = True))
+Ns_todo = Ns.copy()
+logs = [None for _ in Ns]
+
+filename = f'{task_id}_{chain_id}.npz'
+
+if (submit_dir / 'data' / filename).exists():
+    print(f'Job file {filename} already exists')
+    print(f'Loading {filename} to retrieve the work')
+    d = Munch(np.load(submit_dir / 'data' / filename, allow_pickle = True))
     logs = d['logs'][()]
     todo = (logs == None)
     Is_todo = Is_todo[todo]
@@ -119,14 +123,9 @@ if not debug and (submit_dir / 'data' / f'{task_id}.npz').exists():
     
 
 
-(repeat_i, T), = list(islice(config_product, task_id, task_id + 1))
-print(f'repeat_i = {repeat_i}, T = {T}')
+(T,), = list(islice(config_product, task_id, task_id + 1))
+print(f'T = {T}')
     
-if repeat_i > 0:
-    previous_task_id = task_id - len(Ts)
-    MCMC_params.burn_in = 0
-else:
-    previous_task_id = -1
 
 Ham_params.beta = 1 / T
 
@@ -134,7 +133,7 @@ Ham_params.beta = 1 / T
 
 if debug:
     MCMC_params.N_burn_in = 0
-    MCMC_params.N_steps = 500
+    MCMC_params.N_steps = 10
     MCMC_params.thin = 1
     Ham_params.beta = 1 / 2.0 #choose the critical temp where the calculations take longest
     
@@ -142,7 +141,7 @@ if debug:
 
 ##sleep if necessary
 if not debug: 
-    sleeptime = np.random.random() * (30)
+    sleeptime = np.random.random() * (10)
     print(f'Waiting for {sleeptime:.0f} seconds to randomise the finish time')
     sleep(sleeptime)
 
@@ -150,18 +149,22 @@ if not debug:
 ########## Load the previous states ################################################################
 
 #load in the last state from the previous run or use initial_states
-if previous_task_id != -1:
-    print(f'Loading {previous_task_id}.npz to retrieve the last state')
-    d = Munch(np.load(submit_dir / 'data' / f'{previous_task_id}.npz', allow_pickle = True))
-    logs = d['logs'][()]
-    if any(log == None for log in logs):
+if chain_id > 0:
+    MCMC_params.N_burn_in = 0
+    
+    previous_filename = f'{task_id}_{chain_id-1}.npz'
+    
+    print(f'Loading {previous_filename} to retrieve the last state')
+    d = Munch(np.load(submit_dir / 'data' / previous_filename, allow_pickle = True))
+    previous_logs = d['logs'][()]
+    if any(log == None for log in previous_logs):
         print("Previous job didn't finish, exiting")
         raise ValueError
         
-    previous_states = [logs[i].last_state for i, N in enumerate(Ns)]
+    previous_states = {log.N_sites : log.last_state for log in previous_logs}
 else:
     print('Generating initial state as this is the first run with these params')
-    previous_states = [state_factory(N) for N in Ns]
+    previous_states = {N : state_factory(N) for N in Ns_todo}
 
 from FKMC.general import tridiagonal_diagonalisation_benchmark
 benchmark_time = tridiagonal_diagonalisation_benchmark()
@@ -175,35 +178,38 @@ MCMC_params['state'] = '...'
 t0 = time()
 
 for i, N in zip(Is_todo, Ns_todo):
-    if previous_task_id != -1: MCMC_params.N_burn_in = 0
-    MCMC_params.state = previous_states[i]
-
+    MCMC_params.state = previous_states[N]
+    MCMC_params.logger = loggerfactory()
+    
     t0 = time()
     logs[i] = FK_mcmc(**MCMC_params, parameters = Ham_params)
     logs[i].time = time() - t0
     
-    np.savez_compressed(f'{task_id}.npz', 
+    np.savez_compressed(filename, 
         Ns = Ns, parameters = Ham_params, MCMC_params = MCMC_params, 
         structure_names = structure_names,
         structure_dimensions = structure_dimensions,        
+        chain_id = chain_id,
+        task_id = task_id,
         logs = logs, allow_pickle = True,
         desc = ''
         )
     
-    shutil.copy(f'{task_id}.npz', submit_dir / 'data')
+    shutil.copy(filename, submit_dir / 'data')
 
 t = time() - t0
-print(f'{t:.0f} seconds, saving in {Path.cwd()}')
+print(f'{t:.0f} seconds, final saving in {Path.cwd()} / {filename}')
+print(f'Copying to {submit_dir}/data)')
 
 print(f'''
-Requested MCMC steps: {N_steps}
+Requested MCMC steps: {MCMC_params.N_steps}
 Time: {timefmt(t)} 
 Estimated task runtime for 1000 steps: {timefmt(t * 1000 / MCMC_params.N_steps)} 
 ''')
 print('Done')
 
 
-# In[3]:
+# In[ ]:
 
 
 
