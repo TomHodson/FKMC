c92a90fc47f054ab87e52da2f41f77dadfbf2b1e
Tom <tch14@ic.ac.uk>
2020-10-20 16:53:47 +0200

    updates!

---
+++ notebooks/short_paper_figure_code/customcolors.py
@@ -0,0 +1,25 @@
+from matplotlib.colors import to_rgba
+
+colors = list(map(to_rgba, """
+#55115c
+#821760
+#ab275e
+#cd4158
+#e7624f
+#f88646
+#ffad40
+#ffd547
+""".strip().split('\n'))) #https://learnui.design/tools/data-color-picker.html#palette
+
+colors10 = list(map(to_rgba, """
+#55115c
+#7d1560
+#a1225f
+#c1375b
+#db5154
+#eb694d
+#f78347
+#fe9d42
+#ffb941
+#ffd547
+""".strip().split('\n'))) #https://learnui.design/tools/data-color-picker.html#palette
