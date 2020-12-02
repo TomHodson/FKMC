7338d45e3ecc2652f5ba28046de29de32539f1f9
Tom <tch14@ic.ac.uk>
2020-12-02 15:30:38 +0100

    Change the way the figures are styled

--- notebooks/short_paper_figure_code/matplotlib_fig_settings.py
+++ notebooks/short_paper_figure_code/matplotlib_fig_settings.py
@@ -2,12 +2,14 @@ from matplotlib import pyplot as plt
 
 columnwidth = 246/72.27
 
-plt.rc('font', family='sans-serif')
-plt.rc('figure', dpi=200) #makes the plots display larger in the jupyter view
-plt.rc('text', usetex=False)
-plt.rc('xtick', labelsize=8, direction='in', bottom=True, top=True)
-plt.rc('ytick', labelsize=8, direction='in', left=True, right=True)
-plt.rc('axes', labelsize=8)
-plt.rc('figure', autolayout=False)
+#plt.rc('font', family='sans-serif')
+#plt.rc('figure', dpi=200) #makes the plots display larger in the jupyter view
+#plt.rc('text', usetex=False)
+#plt.rc('xtick', labelsize=8, direction='in', bottom=True, top=True)
+#plt.rc('ytick', labelsize=8, direction='in', left=True, right=True)
+#plt.rc('axes', labelsize=8)
+#plt.rc('figure', autolayout=False)
 
+
+plt.style.use('./custom.mplstyle')
 from customcolors import colors, colors10, custom_cmap
